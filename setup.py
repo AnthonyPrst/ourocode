1f498eeb92515aa7e08c85e8d29af4ea429a588d
Anthony Parisot <anthony-p@hotmail.com>
2024-05-18 14:27:24 +0200

    correct name of setup

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("catalog/__version__.py") as fp:
     exec(fp.read(), version)
 
 setup(
-    name="catalog",
+    name="OUREA-EasyCode",
     version=version['__version__'],
     author="Anthony PARISOT",
     author_email="ourea.engineering@gmail.com",
     description="Le but est de créer un catalogue de fonction permettant une utilisation rapide pour la réalisation de note de calcul personnalisée.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Icelone73/Eurocode",
+    url="https://github.com/Icelone73/OUREA-EasyCode",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
